12235c35f0fcb3acc726a581370bb1786072d05d
Defvaiz <defvaiz3@gmail.com>
2022-10-01 03:53:35 +0300

    Описание изменений

---
+++ Project2/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int a;
+	cout << "выберите год" << endl;
+	cin >> a;
+	if (a % 4 == 0)
+	{
+		cout << "год високосный " << endl;
+	}
+	else
+	{
+		cout << "год невисокосный " << endl;
+	}
+
+}
